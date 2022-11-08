e38b850da68ff71704ebf4574a24852d1f59acf5
Developits <nahidnabil1234@gmail.com>
2022-11-08 13:09:33 +0600

    Header added

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link className="btn btn-ghost normal-case text-xl">
+          <img
+            className="w-7 h-7"
+            src="https://cdn4.iconfinder.com/data/icons/camera-7/60/08_Camera_Rotation-512.png"
+            alt=""
+          />
+        </Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal p-0">
+          <li>
+            <Link>Home</Link>
+          </li>
+          <li>
+            <Link>Sign Up</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
