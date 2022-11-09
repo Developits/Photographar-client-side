ae5eeabbb09811b9142e770c66015707fefb3324
Developits <nahidnabil1234@gmail.com>
2022-11-09 13:18:03 +0600

    review section added

--- src/pages/Services/ServiceDetails.js
+++ src/pages/Services/ServiceDetails.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const ServiceDetails = () => {
   const details = useLoaderData();
-  const { title, img, description, price, rating } = details;
+  const { user, loading } = useContext(AuthContext);
+  console.log(user);
+  const { _id, title, img, description, price, rating } = details;
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center">
+        <button className="btn btn-lg btn-ghost loading"></button>
+      </div>
+    );
+  }
+
+  const handleAddReview = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const description = form.message.value;
+    const rating = parseFloat(form.rate.value).toFixed(1);
+
+    const { uid, displayName, photoURL } = user;
+
+    const review = {
+      serviceId: _id,
+      customerId: uid,
+      img: photoURL,
+      name: displayName,
+      rating,
+      description,
+    };
+    fetch("http://localhost:5000/reviews", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(review),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.acknowledged) {
+          alert("Review added successfully");
+          form.reset();
+        }
+      })
+      .catch((er) => console.error(er));
+  };
   return (
     <div>
       {/* Service details section */}
@@ -25,15 +70,39 @@ const ServiceDetails = () => {
       </div>
 
       {/* review section */}
-      <section>
-        <p>description</p>
-        <div>
-          <div>
-            <img src="" alt="" />
+      <div className="w-1/2 mx-auto">
+        <h1 className="text-3xl text-center font-bold">
+          Please, Add your valuable review.
+        </h1>
+        <form onSubmit={handleAddReview} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Write your opinion</span>
+            </label>
+            <textarea
+              name="message"
+              className="textarea textarea-bordered h-24 w-full"
+              placeholder="Your opinion"
+              required
+            ></textarea>
           </div>
-          <div>rating</div>
-        </div>
-      </section>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Rating</span>
+            </label>
+            <input
+              type="text"
+              name="rate"
+              placeholder="Rating"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-2">
+            <input className="btn btn-primary" type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,19 +1,31 @@
 import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import ServiceCard from "./ServiceCard";
+import { AuthContext } from "../../contexts/AuthContext";
 
 const Services = ({ api }) => {
   const [services, setServices] = useState([]);
+  const { loading } = useContext(AuthContext);
+
   useEffect(() => {
     fetch(`http://localhost:5000/${api}`)
       .then((res) => res.json())
       .then((data) => setServices(data));
   }, [api]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center">
+        <button className="btn btn-lg btn-ghost loading"></button>
+      </div>
+    );
+  }
   return (
     <div>
       <div className="text-center mt-16 mb-16">
-        <h2 className="text-5xl text-orange-600 font-semibold">Our Services</h2>
+        <h2 className="text-5xl mb-4 text-orange-600 font-semibold">
+          Our Services
+        </h2>
         <p>
           We exists to create value and impact through Films and Photography.
           With over 1000+ videos, we combine our knowledge of Film-making to
